5d0f845a105e4a25c6a1a3a1920583a7ac69f8a5
Julian <julian@Administrators-MacBook-Pro.local>
2023-06-22 21:15:27 -0400

    server test

--- server.js
+++ server.js
@@ -16,30 +16,30 @@
 // app.use(express.static("public"));
 
 // app.get("/", (req, res) => {
-//   // const city = req.query.city;
+//   const city = req.query.city;
 
-//   // if (!city) {
-//   //   // Render the initial form if no city is provided
-//   //   res.render("aqi", { aqiData: null });
-//   //   return;
-//   // }
+//   if (!city) {
+//     // Render the initial form if no city is provided
+//     res.render("aqi", { aqiData: null });
+//     return;
+//   }
 
-//   // // Construct the API URL
-//   // const apiUrl = `https://api.waqi.info/feed/${city}/?token=3384b09b33dece368cea032594de015cab94b9b0`;
+//   // Construct the API URL
+//   const apiUrl = `https://api.waqi.info/feed/${city}/?token=3384b09b33dece368cea032594de015cab94b9b0`;
 
-//   // // Make a request to the AQI API
-//   // fetch(apiUrl)
-//   //   .then((response) => response.json())
-//   //   .then((data) => {
-//   //     const aqiData = data.data; // Extract the AQI data from the API response
-//   //     res.render("aqi", { aqiData }); // Pass the AQI data to the view for rendering
-//   //   })
-//   //   .catch((error) => {
-//   //     console.log("Error:", error);
-//   //     res.status(500).json({ error: "An error occurred" });
-//     // });
+//   // Make a request to the AQI API
+//   fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const aqiData = data.data; // Extract the AQI data from the API response
+//       res.render("aqi", { aqiData }); // Pass the AQI data to the view for rendering
+//     })
+//     .catch((error) => {
+//       console.log("Error:", error);
+//       res.status(500).json({ error: "An error occurred" });
+//     });
 
-//   // julian can use the functions he writes in the mysql.js here to query the database
+//   //julian can use the functions he writes in the mysql.js here to query the database
 //   //result =  getAdvisoriesForAQI(response.value)
 // });
 
