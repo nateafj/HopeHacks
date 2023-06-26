665f3ed7e969ed37c254e83e84cdc8c7dc6d3839
ford <ford@fords-MacBook-Pro.local>
2023-06-26 13:22:46 -0400

    ejs

--- app.js
+++ app.js
@@ -5,21 +5,22 @@ const fetch = require('node-fetch');
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+const airQualityData = {
+  aqi: null,
+  category: null,
+  pollutants: [],
+};
 
-app.get('/', (req, res) => {
+app.get('/airQuality', (req, res) => {
   const apiKey = '08D5FF0B-FE8E-4121-B16B-D46CC14D62AA';
-  const zipCode = req.query.city; // Example: set the desired ZIP code
+  const zipCode = req.query.zipcode; // Example: set the desired ZIP code
 
   const url = `https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${zipCode}&API_KEY=${apiKey}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const airQualityData = {
-        aqi: null,
-        category: null,
-        pollutants: [],
-      };
+      
 
       if (data.length > 0) {
         const firstDataPoint = data[0];
@@ -37,8 +38,7 @@ app.get('/', (req, res) => {
           airQualityData.pollutants.push(pollutant);
         });
       }
-
-      res.render('aqi', { airQualityData });
+      res.render('airQuality', { airQualityData });
       console.log(airQualityData);
     })
     .catch(error => {
@@ -46,6 +46,12 @@ app.get('/', (req, res) => {
       res.status(500).json({ error: 'An error occurred' });
     });
 });
+
+app.get('/', (req, res)=> {
+  console.log("test")
+  console.log(airQualityData)
+  res.render('home', { airQualityData });
+}) 
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
