a913b2d80e0602d0d04876653385b83a64058807
abheek786 <rabheek@gmail.com>
2023-06-26 13:48:47 -0400

    fix: path issue

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const fetch = require('node-fetch');
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-const airQualityData = {
-  aqi: null,
-  category: null,
-  pollutants: [],
-};
+app.get('/', (req, res) => {
+  res.render('home', { });
+});
 
 app.get('/airQuality', (req, res) => {
   const apiKey = '08D5FF0B-FE8E-4121-B16B-D46CC14D62AA';
@@ -20,13 +18,20 @@ app.get('/airQuality', (req, res) => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      
+      const airQualityData = {
+        aqi: null,
+        category: null,
+        pollutants: []
+      };
 
       if (data.length > 0) {
         const firstDataPoint = data[0];
         airQualityData.aqi = firstDataPoint.AQI;
         airQualityData.category = firstDataPoint.Category?.Name;
         airQualityData.categoryNumber = firstDataPoint.Category?.Number;
+
+        // Read thev json where you will have the custom message: https://www.geeksforgeeks.org/how-to-read-and-write-json-file-using-node-js/
+        // fetch that specfic message from the JSOn and append it to the airQualiy Data
 
         data.forEach(item => {
           const pollutant = {
@@ -39,19 +44,12 @@ app.get('/airQuality', (req, res) => {
         });
       }
       res.render('airQuality', { airQualityData });
-      console.log(airQualityData);
     })
     .catch(error => {
       console.error('Error:', error);
       res.status(500).json({ error: 'An error occurred' });
     });
 });
-
-app.get('/', (req, res)=> {
-  console.log("test")
-  console.log(airQualityData)
-  res.render('home', { airQualityData });
-}) 
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
